e0ff9550f33df3f38500c936499fabc4c46ab9b5
Maxhy <maxime@leosac.com>
2026-04-29 09:38:36 +0200

    Add SSL listening options

--- src/run.js
+++ src/run.js
@@ -1,8 +1,24 @@
 require('dotenv').config();
 const app = require('./app.js');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 
 const hostname = process.env.HOSTNAME || "127.0.0.1";
 const port = process.env.PORT || 4000;
-app.listen(port, hostname, () => {
+
+http.createServer(app).listen(port, hostname, () => {
     console.log(`listening on ${hostname}:${port} with logging level '${process.env.LOGGING_LEVEL}'...`);
 });
+if (process.env.HTTPS_ENABLED === 'true') {
+    var privateKey = fs.readFileSync(process.env.SSL_KEY);
+    var certificate = fs.readFileSync(process.env.SSL_CERT);
+    const options = {
+        key: privateKey,
+        cert: certificate
+    }
+    const port_ssl = process.env.PORT_SSL || 4001;
+    https.createServer(options, app).listen(port_ssl, hostname, () => {
+        console.log(`listening on ${hostname}:${port_ssl} with logging level '${process.env.LOGGING_LEVEL}'...`);
+    });
+}
